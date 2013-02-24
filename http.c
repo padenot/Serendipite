f19cbb258e09c220ad3c749230e23711a11f4479
Paul ADENOT <paul@paul.cx>
2013-02-24 14:24:19 +0100

    http cleanup, separate .c .h

--- http.c
+++ http.c
@@ -5,27 +5,32 @@
 #include <unistd.h>
 #include "macros.h"
 
-const char* LFM_HOST = "ws.audioscrobbler.com";
-
+#define LFM_HOST "ws.audioscrobbler.com"
 #define PACKAGE_STRING "Serendipite 0.1"
-
 #define API_KEY "d4ff4069de5ae7929d4037dc01313301"
 
-#define request_text "GET %s HTTP/1.1\r\n" \
-                     "Host: ws.audioscrobbler.com\r\n" \
-                     "User-Agent: " \
-                     PACKAGE_STRING \
-                     "\r\n" \
-                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" \
-                     "Accept-Language: en-US,en;q=0.5\r\n\r\n"
+#define REQUEST_TEXT                                                           \
+"GET %s HTTP/1.1\r\n"                                                          \
+"Host: %s\r\n"                                                                 \
+"User-Agent: "                                                                 \
+PACKAGE_STRING                                                                 \
+"\r\n"                                                                         \
+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"  \
+"Accept-Language: en-US,en;q=0.5\r\n\r\n"
 
-#define new_release_url "/2.0/?method=user.getnewreleases&user=evok3r&api_key=" API_KEY "&format=json"
+#define NEW_RELEASE_URL                                                        \
+"/2.0/?method=user.getnewreleases"                                             \
+"&user=evok3r&api_key="                                                        \
+API_KEY                                                                        \
+"&format=json"
 
-int request(const char* url, size_t length, char** request, size_t* request_length)
+void http_request(const char* url, size_t url_length, const char* host,
+                  size_t host_length, char** request, size_t* request_length)
 {
   ASSERT(request && request_length, "NULL request or request_length");
-  /* size of the request boilerplate + size of the url - strlen("%s") + NULL byte */
-  *request_length = length + strlen(request_text) - 1;
+  /* size of the request string + size of the url
+   * + size of the host - strlen("%s") * 2 + NULL byte */
+  *request_length = url_length + host_length + strlen(REQUEST_TEXT) - 3;
   *request = malloc(*request_length);
 
   if (!*request) {
@@ -33,17 +38,12 @@ int request(const char* url, size_t length, char** request, size_t* request_leng
     exit(1);
   }
 
-  LOG(LOG_DEBUG, "%s", request_text);
-
-  sprintf(*request, request_text, url);
-  LOG(LOG_DEBUG, "%s", *request);
-
-  return 0;
+  sprintf(*request, REQUEST_TEXT, url, host);
 }
 
-int socket_connect()
+int http_connect(const char* host)
 {
-  int socket_fd, s;
+  int socket_fd, rv;
   struct addrinfo h = {0};
   struct addrinfo* r;
 
@@ -52,8 +52,8 @@ int socket_connect()
   h.ai_flags = 0;
   h.ai_protocol = 0;
 
-  if ((s = getaddrinfo(LFM_HOST, "80", &h, &r))) {
-    LOG(LOG_FATAL, "getaddrinfo: %s\n", gai_strerror(s));
+  if ((rv = getaddrinfo(LFM_HOST, "80", &h, &r))) {
+    LOG(LOG_FATAL, "getaddrinfo: %s\n", gai_strerror(rv));
     return ERROR;
   }
 
@@ -63,7 +63,7 @@ int socket_connect()
     return ERROR;
   }
 
-  if ((s = connect(socket_fd, r->ai_addr, r->ai_addrlen)) == -1) {
+  if ((rv = connect(socket_fd, r->ai_addr, r->ai_addrlen)) == -1) {
     int e = errno;
     LOG(LOG_FATAL, "connect() error: %s", strerror(e));
     return ERROR;
@@ -73,12 +73,12 @@ int socket_connect()
   return socket_fd;
 }
 
-int socket_close(int fd)
+int http_close(int fd)
 {
   return close(fd);
 }
 
-int socket_send_request(int socket_fd, const char* request, size_t length)
+int http_send_request(int socket_fd, const char* request, size_t length)
 {
   int rv = write(socket_fd, request, length);
   if (rv == -1) {
@@ -88,7 +88,7 @@ int socket_send_request(int socket_fd, const char* request, size_t length)
   return rv;
 }
 
-int socket_read_response(int socket_fd, char** buffer, size_t* length)
+int http_read_response(int socket_fd, char** buffer, size_t* length)
 {
   int rv;
   int offset = 0;
@@ -96,7 +96,7 @@ int socket_read_response(int socket_fd, char** buffer, size_t* length)
   int read_size = 1024;
   ASSERT(buffer && length, "NULL buffer or length");
 
-  *buffer = malloc(16 * 1024);
+  *buffer = malloc(buffer_size);
 
   while((rv = read(socket_fd, *buffer + offset, read_size))) {
     offset += rv;
@@ -111,11 +111,6 @@ int socket_read_response(int socket_fd, char** buffer, size_t* length)
     }
   }
 
-  if (rv == -1) {
-    int e = errno;
-    LOG(LOG_CRITICAL, "read() error: %s", strerror(e));
-  }
-
   offset += rv;
   (*buffer)[offset] = 0;
 
@@ -124,23 +119,24 @@ int socket_read_response(int socket_fd, char** buffer, size_t* length)
 
 int main(int argc, char* argv[])
 {
-  int socket_fd = socket_connect();
+  int socket_fd = http_connect(LFM_HOST);
   size_t length, request_text_length;
   char* buffer;
 
-  request(new_release_url, strlen(new_release_url), &buffer, &request_text_length);
+  http_request(NEW_RELEASE_URL, strlen(NEW_RELEASE_URL),
+          LFM_HOST, strlen(LFM_HOST), &buffer, &request_text_length);
 
-  socket_send_request(socket_fd, buffer, strlen(buffer));
+  http_send_request(socket_fd, buffer, strlen(buffer));
 
   free(buffer);
 
-  socket_read_response(socket_fd, &buffer, &length);
+  http_read_response(socket_fd, &buffer, &length);
 
   puts(buffer);
 
   free(buffer);
 
-  socket_close(socket_fd);
+  http_close(socket_fd);
 
   return OK;
 }
---
+++ http.h
@@ -0,0 +1,26 @@
+/**
+ * Connect to `host` on port 80.
+ * returns a socket fd.
+ */
+int http_connect(const char* host);
+/**
+ *  Close the fd `fd`.
+ */
+int http_close(int fd)
+/**
+ * Crafts an http request for the url `url`, the host `host`, of respective size
+ * `url_length` and `host_length`. `request` will be allocated, and its size
+ * will be `request_length`.
+ */
+int http_request(const char* url, size_t url_length,
+                 const char* host, size_t host_length,
+                 char** request, size_t* request_length);
+/**
+ * Send the http request `request` of length `length` to the socket `fd`.
+ */
+int http_send_request(int fd, const char* request, size_t length)
+/**
+ * Read the response on the socket `fd`. `buffer` will be allocated, and shall
+ * be freed by the user. `length` is the number of bytes read.
+ */
+int http_read_response(int fd, char** buffer, size_t* length)
